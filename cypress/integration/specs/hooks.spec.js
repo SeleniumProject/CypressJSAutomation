4b6e17dfe99afef4f7800ef701809b4083d5b093
kudikalr <Ramesh.Kudikala@cdk.com>
2020-08-24 21:58:14 +0530

    commit with latest changes

--- cypress/integration/specs/hooks.spec.js
+++ cypress/integration/specs/hooks.spec.js
@@ -1,33 +1,33 @@
-const { idText, createYield } = require("typescript")
+// const { idText, createYield } = require("typescript")
 
-beforeEach(() => {
-    // root-level hook
-    // runs before every test
-  })
+// beforeEach(() => {
+//     // root-level hook
+//     // runs before every test
+//   })
   
-  describe('Hooks', function() {
-    before(() => {
-      // runs once before all tests in the block
-      cy.fixture('example').then(function(data){
-            this.data =data;
-      })
-    })
+//   describe('Hooks', function() {
+//     before(() => {
+//       // runs once before all tests in the block
+//       cy.fixture('example').then(function(data){
+//             this.data =data;
+//       })
+//     })
   
-    it('some data transfer', function(){
+//     it('some data transfer', function(){
 
-        cy.visit(this.data.name)
-        cy.get('.ico-login').click()
-        cy.get('#Email').type(this.data.email)
-    })
-    beforeEach(() => {
-      // runs before each test in the block
-    })
+//         cy.visit(this.data.name)
+//         cy.get('.ico-login').click()
+//         cy.get('#Email').type(this.data.email)
+//     })
+//     beforeEach(() => {
+//       // runs before each test in the block
+//     })
   
-    afterEach(() => {
-      // runs after each test in the block
-    })
+//     afterEach(() => {
+//       // runs after each test in the block
+//     })
   
-    after(() => {
-      // runs once after all tests in the block
-    })
-  })
+//     after(() => {
+//       // runs once after all tests in the block
+//     })
+//   })
